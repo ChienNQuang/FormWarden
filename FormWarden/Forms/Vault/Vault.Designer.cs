86102e05490cd796e5c1c4ac80206a3edfc2e2e7
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-07-13 11:58:51 +0700

    fix

--- FormWarden/Forms/Vault/Vault.Designer.cs
+++ FormWarden/Forms/Vault/Vault.Designer.cs
@@ -28,141 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvIdentities = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnNewIdentity = new System.Windows.Forms.Button();
-            this.btnSignOut = new System.Windows.Forms.Button();
-            this.CategoryCbx = new System.Windows.Forms.ComboBox();
-            this.newCategoryBtn = new System.Windows.Forms.Button();
-            this.deleteCategoryBtn = new System.Windows.Forms.Button();
-            this.btnExport = new System.Windows.Forms.Button();
-            this.btnImport = new System.Windows.Forms.Button();
-            this.btGenerator = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvIdentities)).BeginInit();
-            this.SuspendLayout();
+            dgvIdentities = new DataGridView();
+            label1 = new Label();
+            btnNewIdentity = new Button();
+            btnSignOut = new Button();
+            CategoryCbx = new ComboBox();
+            newCategoryBtn = new Button();
+            deleteCategoryBtn = new Button();
+            btnExport = new Button();
+            btnImport = new Button();
+            btGenerator = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvIdentities).BeginInit();
+            SuspendLayout();
             // 
             // dgvIdentities
             // 
-            this.dgvIdentities.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvIdentities.Location = new System.Drawing.Point(167, 72);
-            this.dgvIdentities.Name = "dgvIdentities";
-            this.dgvIdentities.RowTemplate.Height = 25;
-            this.dgvIdentities.Size = new System.Drawing.Size(795, 338);
-            this.dgvIdentities.TabIndex = 0;
-            this.dgvIdentities.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvIdentities_CellContentDoubleClick);
+            dgvIdentities.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvIdentities.Location = new Point(167, 72);
+            dgvIdentities.Name = "dgvIdentities";
+            dgvIdentities.RowTemplate.Height = 25;
+            dgvIdentities.Size = new Size(795, 338);
+            dgvIdentities.TabIndex = 0;
+            dgvIdentities.CellContentDoubleClick += dgvIdentities_CellContentDoubleClick;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(167, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 37);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "All vaults";
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(167, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 37);
+            label1.TabIndex = 1;
+            label1.Text = "All vaults";
             // 
             // btnNewIdentity
             // 
-            this.btnNewIdentity.BackColor = System.Drawing.SystemColors.Highlight;
-            this.btnNewIdentity.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnNewIdentity.ForeColor = System.Drawing.Color.Transparent;
-            this.btnNewIdentity.Location = new System.Drawing.Point(851, 22);
-            this.btnNewIdentity.Name = "btnNewIdentity";
-            this.btnNewIdentity.Size = new System.Drawing.Size(111, 37);
-            this.btnNewIdentity.TabIndex = 2;
-            this.btnNewIdentity.Text = "+ New item";
-            this.btnNewIdentity.UseVisualStyleBackColor = false;
+            btnNewIdentity.BackColor = SystemColors.Highlight;
+            btnNewIdentity.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnNewIdentity.ForeColor = Color.Transparent;
+            btnNewIdentity.Location = new Point(851, 22);
+            btnNewIdentity.Name = "btnNewIdentity";
+            btnNewIdentity.Size = new Size(111, 37);
+            btnNewIdentity.TabIndex = 2;
+            btnNewIdentity.Text = "+ New item";
+            btnNewIdentity.UseVisualStyleBackColor = false;
+            btnNewIdentity.Click += btnNewIdentity_Click;
             // 
             // btnSignOut
             // 
-            this.btnSignOut.Location = new System.Drawing.Point(12, 72);
-            this.btnSignOut.Name = "btnSignOut";
-            this.btnSignOut.Size = new System.Drawing.Size(121, 56);
-            this.btnSignOut.TabIndex = 3;
-            this.btnSignOut.Text = "Sign out";
-            this.btnSignOut.UseVisualStyleBackColor = true;
+            btnSignOut.Location = new Point(12, 72);
+            btnSignOut.Name = "btnSignOut";
+            btnSignOut.Size = new Size(121, 56);
+            btnSignOut.TabIndex = 3;
+            btnSignOut.Text = "Sign out";
+            btnSignOut.UseVisualStyleBackColor = true;
+            btnSignOut.Click += btnSignOut_Click;
             // 
             // CategoryCbx
             // 
-            this.CategoryCbx.FormattingEnabled = true;
-            this.CategoryCbx.Location = new System.Drawing.Point(12, 235);
-            this.CategoryCbx.Name = "CategoryCbx";
-            this.CategoryCbx.Size = new System.Drawing.Size(121, 23);
-            this.CategoryCbx.TabIndex = 4;
+            CategoryCbx.FormattingEnabled = true;
+            CategoryCbx.Location = new Point(12, 235);
+            CategoryCbx.Name = "CategoryCbx";
+            CategoryCbx.Size = new Size(121, 23);
+            CategoryCbx.TabIndex = 4;
             // 
             // newCategoryBtn
             // 
-            this.newCategoryBtn.BackColor = System.Drawing.SystemColors.Highlight;
-            this.newCategoryBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.newCategoryBtn.ForeColor = System.Drawing.Color.Transparent;
-            this.newCategoryBtn.Location = new System.Drawing.Point(715, 22);
-            this.newCategoryBtn.Name = "newCategoryBtn";
-            this.newCategoryBtn.Size = new System.Drawing.Size(130, 37);
-            this.newCategoryBtn.TabIndex = 5;
-            this.newCategoryBtn.Text = "+ New category";
-            this.newCategoryBtn.UseVisualStyleBackColor = false;
+            newCategoryBtn.BackColor = SystemColors.Highlight;
+            newCategoryBtn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            newCategoryBtn.ForeColor = Color.Transparent;
+            newCategoryBtn.Location = new Point(715, 22);
+            newCategoryBtn.Name = "newCategoryBtn";
+            newCategoryBtn.Size = new Size(130, 37);
+            newCategoryBtn.TabIndex = 5;
+            newCategoryBtn.Text = "+ New category";
+            newCategoryBtn.UseVisualStyleBackColor = false;
+            newCategoryBtn.Click += newCategoryBtn_Click;
             // 
             // deleteCategoryBtn
             // 
-            this.deleteCategoryBtn.BackColor = System.Drawing.SystemColors.Highlight;
-            this.deleteCategoryBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.deleteCategoryBtn.ForeColor = System.Drawing.Color.Transparent;
-            this.deleteCategoryBtn.Location = new System.Drawing.Point(572, 22);
-            this.deleteCategoryBtn.Name = "deleteCategoryBtn";
-            this.deleteCategoryBtn.Size = new System.Drawing.Size(137, 37);
-            this.deleteCategoryBtn.TabIndex = 6;
-            this.deleteCategoryBtn.Text = "- Delete category";
-            this.deleteCategoryBtn.UseVisualStyleBackColor = false;
+            deleteCategoryBtn.BackColor = SystemColors.Highlight;
+            deleteCategoryBtn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            deleteCategoryBtn.ForeColor = Color.Transparent;
+            deleteCategoryBtn.Location = new Point(572, 22);
+            deleteCategoryBtn.Name = "deleteCategoryBtn";
+            deleteCategoryBtn.Size = new Size(137, 37);
+            deleteCategoryBtn.TabIndex = 6;
+            deleteCategoryBtn.Text = "- Delete category";
+            deleteCategoryBtn.UseVisualStyleBackColor = false;
+            deleteCategoryBtn.Click += deleteCategoryBtn_Click;
             // 
             // btnExport
             // 
-            this.btnExport.Location = new System.Drawing.Point(12, 331);
-            this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(121, 23);
-            this.btnExport.TabIndex = 7;
-            this.btnExport.Text = "Export vault";
-            this.btnExport.UseVisualStyleBackColor = true;
+            btnExport.Location = new Point(12, 331);
+            btnExport.Name = "btnExport";
+            btnExport.Size = new Size(121, 23);
+            btnExport.TabIndex = 7;
+            btnExport.Text = "Export vault";
+            btnExport.UseVisualStyleBackColor = true;
+            btnExport.Click += btnExport_Click;
             // 
             // btnImport
             // 
-            this.btnImport.Location = new System.Drawing.Point(12, 290);
-            this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(121, 23);
-            this.btnImport.TabIndex = 8;
-            this.btnImport.Text = "Import file";
-            this.btnImport.UseVisualStyleBackColor = true;
+            btnImport.Location = new Point(12, 290);
+            btnImport.Name = "btnImport";
+            btnImport.Size = new Size(121, 23);
+            btnImport.TabIndex = 8;
+            btnImport.Text = "Import file";
+            btnImport.UseVisualStyleBackColor = true;
+            btnImport.Click += btnImport_Click;
             // 
             // btGenerator
             // 
-            this.btGenerator.Location = new System.Drawing.Point(12, 144);
-            this.btGenerator.Name = "btGenerator";
-            this.btGenerator.Size = new System.Drawing.Size(121, 59);
-            this.btGenerator.TabIndex = 9;
-            this.btGenerator.Text = "Generator";
-            this.btGenerator.UseVisualStyleBackColor = true;
-            this.btGenerator.Click += new System.EventHandler(this.btGenerator_Click);
+            btGenerator.Location = new Point(12, 144);
+            btGenerator.Name = "btGenerator";
+            btGenerator.Size = new Size(121, 59);
+            btGenerator.TabIndex = 9;
+            btGenerator.Text = "Generator";
+            btGenerator.UseVisualStyleBackColor = true;
+            btGenerator.Click += btGenerator_Click;
             // 
             // Vault
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1005, 450);
-            this.Controls.Add(this.btGenerator);
-            this.Controls.Add(this.btnImport);
-            this.Controls.Add(this.btnExport);
-            this.Controls.Add(this.deleteCategoryBtn);
-            this.Controls.Add(this.newCategoryBtn);
-            this.Controls.Add(this.CategoryCbx);
-            this.Controls.Add(this.btnSignOut);
-            this.Controls.Add(this.btnNewIdentity);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dgvIdentities);
-            this.Name = "Vault";
-            this.Text = "Vault";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvIdentities)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1005, 450);
+            Controls.Add(btGenerator);
+            Controls.Add(btnImport);
+            Controls.Add(btnExport);
+            Controls.Add(deleteCategoryBtn);
+            Controls.Add(newCategoryBtn);
+            Controls.Add(CategoryCbx);
+            Controls.Add(btnSignOut);
+            Controls.Add(btnNewIdentity);
+            Controls.Add(label1);
+            Controls.Add(dgvIdentities);
+            Name = "Vault";
+            Text = "Vault";
+            FormClosing += Vault_FormClosing;
+            ((System.ComponentModel.ISupportInitialize)dgvIdentities).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
